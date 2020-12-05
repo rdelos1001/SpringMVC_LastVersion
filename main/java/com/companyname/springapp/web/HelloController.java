451d1afac48fec503dd328436945675a603ec8ba
raul <r.delos@hotmail.com>
2020-12-05 15:23:35 +0100

    apartado2

--- main/java/com/companyname/springapp/web/HelloController.java
+++ main/java/com/companyname/springapp/web/HelloController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.util.Date;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -20,8 +20,9 @@ public class HelloController {
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        logger.info("Returning hello view");
+        String now = (new Date()).toString();
+        logger.info("Returning hello view with " + now);
 
-        return new ModelAndView("hello.jsp");
+        return new ModelAndView("hello", "now", now);
     }
 }
--- test/java/com/companyname/springapp/web/HelloControllerTest.java
+++ test/java/com/companyname/springapp/web/HelloControllerTest.java
@@ -12,6 +12,9 @@ public class HelloControllerTest {
     public void testHandleRequestView() throws Exception{		
         HelloController controller = new HelloController();
         ModelAndView modelAndView = controller.handleRequest(null, null);		
-        assertEquals("hello.jsp", modelAndView.getViewName());
+        assertEquals("hello", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        String nowValue = (String) modelAndView.getModel().get("now");
+        assertNotNull(nowValue);
     }
 }
