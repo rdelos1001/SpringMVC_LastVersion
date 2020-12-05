7e524317c22b3b226570159fa0a4d64b0985761b
raul <r.delos@hotmail.com>
2020-12-05 18:28:25 +0100

    apartado4

--- main/java/com/companyname/springapp/web/InventoryController.java
+++ main/java/com/companyname/springapp/web/InventoryController.java
@@ -5,17 +5,26 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.companyname.springapp.service.ProductManager;
+
 @Controller
-public class HelloController {
+public class InventoryController {
 
     protected final Log logger = LogFactory.getLog(getClass());
-
+    
+    @Autowired
+    private ProductManager productManager;
+    
     @RequestMapping(value="/hello.htm")
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -23,6 +32,15 @@ public class HelloController {
         String now = (new Date()).toString();
         logger.info("Returning hello view with " + now);
 
-        return new ModelAndView("hello", "now", now);
+        Map<String, Object> myModel = new HashMap<String, Object>();
+        myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
+
+        return new ModelAndView("hello", "model", myModel);
+    }
+
+
+    public void setProductManager(ProductManager productManager) {
+        this.productManager = productManager;
     }
 }
---
+++ test/java/com/companyname/springapp/web/InventoryControllerTest.java
@@ -0,0 +1,28 @@
+package com.companyname.springapp.web;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.companyname.springapp.service.SimpleProductManager;
+
+
+public class InventoryControllerTest {
+
+    @Test
+    public void testHandleRequestView() throws Exception{		
+        InventoryController controller = new InventoryController();
+        controller.setProductManager(new SimpleProductManager());
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("hello", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        
+    	@SuppressWarnings("unchecked")
+        Map<String, Object> modelMap = (Map<String, Object>) modelAndView.getModel().get("model");
+        String nowValue = (String) modelMap.get("now");
+        assertNotNull(nowValue);
+    }
+}
